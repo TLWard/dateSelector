861b413540d328e143c5a4dfe9f1745d6650bf82
Tommy Ward <41960269+TLWard@users.noreply.github.com>
2019-01-03 19:32:09 -0500

    Add files via upload

---
+++ es/DateUtilities.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export function GetDateString(dte) {
+
+    return dte.toISOString().split('T')[0];
+}
+export function GetDate(dte) {
+
+    return new Date(dte);
+}
